4d18d27ebb081a313edc1bda34b0050f48abbd7f
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-08-12 00:07:15 +0530

    Add files via upload

---
+++ Login Form/flip.js
@@ -0,0 +1,14 @@
+// flip animation
+//-------------------------------------------------------------
+     var card = document.getElementById('card');
+        
+     function OR(){
+         card.style.transform = "rotateY(-180deg)";
+     }
+     function OL(){
+         card.style.transform = "rotateY(-0deg)";  
+     }
+     
+//---------------------------------------------------------------
+
+
