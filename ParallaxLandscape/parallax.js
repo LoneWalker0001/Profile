5f540af9cb0779f69e6ffd6cd97562a2f5a04f3c
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-08-16 17:15:31 +0530

    Update parallax.js

--- ParallaxLandscape/parallax.js
+++ ParallaxLandscape/parallax.js
@@ -5,9 +5,9 @@ let bird = document.getElementById("B2");
 let sea = document.getElementById( "S2");
 window.addEventListener( 'scroll' , function(){
 var value = window.scrollY;
-bg.style.top = value*0.8 +'px';
-cloud.style.left = -value * 0.7 + 'px';
-bird.style.left = value * 0.8 + 'px';
-mountain.style.top = value * 0.22 + ' px' ;
-sea.style.top = value * 0.20 + 'px' ;
+bg.style.top = value*0.5 +'px';
+cloud.style.left = -value * 0.3 + 'px';
+bird.style.left = value * 0.6 + 'px';
+mountain.style.top = value * 0.15 + ' px' ;
+sea.style.top = value * 0.16 + 'px' ;
 })
