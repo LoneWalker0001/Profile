d9de9c5ab27e28fb1150e3d2b302cd049e497774
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-09-14 14:06:11 +0530

    Update Navscript.js

--- Parallax Landscape/Navscript.js
+++ Parallax Landscape/Navscript.js
@@ -1,6 +1,11 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('focus', () => {
-  navbarLinks.classList.toggle('active')
+const hamburger = document.querySelector(" .hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click" , () => {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
 })
+document.querySelectorAll(" .nav-link").forEach(n => n.
+addEventListener( "click", () => {
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+}))
