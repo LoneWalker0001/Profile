eec8a1c599b179a815a4ea9eb99d9b41ba681d74
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-09-24 17:00:29 +0530

    Add files via upload

---
+++ Main Portfolio/JS/Feedback.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwa_vULKGFo_ybvRztPvSPZbc4WBsGfe4NDyI62RNmnFbpLsjC3OPujiY4mkK8aoa6K4A/exec'
+const form = document.forms['Profile One Feedback']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => document.getElementById("indicate").innerHTML="Thanks for Your feed back")
+    .catch(error => console.error('Error!', error.message))
+})
---
+++ Main Portfolio/JS/autotype.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#auto-input",{
+    strings: ["   Hello", "   I'm Faizan Mirza", "   I'm Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+    })
