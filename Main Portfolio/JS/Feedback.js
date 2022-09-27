a7da4d58aaac94a2c1134e575d0bee4ff97a8d21
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-09-27 14:21:18 +0530

    Update Feedback.js

--- Main Portfolio/JS/Feedback.js
+++ Main Portfolio/JS/Feedback.js
@@ -4,6 +4,6 @@ const form = document.forms['Profile One Feedback']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => document.getElementById("indicate").innerHTML="fthfjg";alert("Thanks for Your feedback"))
+    .then(response => document.getElementById("indicate").innerHTML="Your feedback is saved in our record" , alert="Thanks for Your feedback")
     .catch(error => console.error('Error!', error.message))
 })
