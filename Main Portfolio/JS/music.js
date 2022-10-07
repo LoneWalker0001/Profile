0aecbb095b2e6338c4da5c6f529f6b370f3c9254
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2022-10-07 22:38:11 +0530

    Update music.js

--- Main Portfolio/JS/music.js
+++ Main Portfolio/JS/music.js
@@ -55,7 +55,7 @@ $(function () {
         "ILLENIUM",
         "SubSpace & RITIX",
         "Alan Walker x Benjamin"
-        "Bhuvan Bam",
+        "Bhuvan Bam"
       ],
       albumArtworks = ["_1", "_2", "_3", "_4", "_5","_6","_7", "_8", "_9", "_10","_11","_12"],
       trackUrl = [
