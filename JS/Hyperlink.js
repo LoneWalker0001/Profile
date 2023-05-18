a89c12319c0f942616f046d0f146c9481d2e4d6f
Lone Walker <102238908+LoneWalker0001@users.noreply.github.com>
2023-05-18 11:41:48 +0530

    Update Hyperlink.js

--- JS/Hyperlink.js
+++ JS/Hyperlink.js
@@ -11,6 +11,12 @@ function Library(link){
     location.href = './Library.html';
 }
 
-document.getElementById('play').addEventListener('click', ()=>{
-     location.href = './Library.html';
+// document.getElementById('play').addEventListener('click', ()=>{
+//      location.href = './Library.html';
+// })
+// document.querySelectorAll('.play-pause').forEach(()=>{
+//     // Now do something with my button
+// });
+document.getElementsByClassName('play-pause')].forEach(el => {
+    location.href = './Library.html';
 })
